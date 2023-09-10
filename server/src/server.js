a8a29073104c038fb74e927ce71b4d3d89f67997
abtyagi15 <abtyagi150702@gmail.com>
2023-09-11 02:25:56 +0530

    added middleware to handle requests

--- server/src/server.js
+++ server/src/server.js
@@ -1,49 +1,13 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-const { spawn } = require("child_process");
+const predict = require("./routes/predictRoute");
 require("dotenv").config();
 //database connection
 const dbConnect = require("./configuration/database");
 dbConnect();
 
-const filePath = path.join(__dirname, "/ML_Part/classify.py");
-
-// Define the Python script and its arguments
-const pythonScript = filePath;
-
 app.use(express.json());
-// Define a route for making predictions
-app.post("/predict", (req, res) => {
-
-  // Get the input data from the request body 
-  // const inputJson = JSON.stringify(req.body);
-  const  data = req.body;
- 
-  const inputJson = JSON.stringify(data); // Replace with your input data
-
-  console.log("This is sent by postman",data);
-
-  // Spawn the Python child process
-  const pythonProcess = spawn("python", [pythonScript, inputJson]);
-
-  // Handle the output from the Python process
-  pythonProcess.stdout.on("data", (data) => {
-    const result = JSON.parse(data.toString());
-    console.log(`Python script output: ${JSON.stringify(result)}`);
-    res.send(result);
-  });
-
-  // Handle errors, if any
-  pythonProcess.stderr.on("data", (data) => {
-    console.error(`Python script error: ${data.toString()}`);
-  });
-
-  // Listen for the Python process to exit
-  pythonProcess.on("close", (code) => {
-    console.log(`Python script exited with code ${code}`);
-  });
-});
+app.use('/campusnavigator/v1',predict)
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
